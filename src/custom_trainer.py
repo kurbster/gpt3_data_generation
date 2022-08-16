170d23fba50cdf34e644e8d2a8230a8ecb533095
kurbster <kkuznia28@gmail.com>
2022-08-15 22:07:05 -0700

    Init commit.

---
+++ src/custom_trainer.py
@@ -0,0 +1,101 @@
+import json
+import logging
+
+from dataclasses import dataclass
+from typing import Optional, Union
+from pathlib import Path
+
+import torch
+from transformers import Trainer, TrainerCallback
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase, PaddingStrategy
+
+
+logger =  logging.getLogger("transformers.trainer")
+eval_logger = logging.getLogger("evaluation")
+
+class LogCallBack(TrainerCallback):
+    def on_log(self, args, state, control, logs=None, **kwargs):
+        if state.is_local_process_zero:
+            logger.debug(f'I am the logs: {logs}')
+            if "eval_accuracy" in logs:
+                eval_output_path = Path(args.output_dir, 'evaluation_results')
+                epoch = 1 if state.epoch is None else int(state.epoch)
+                fname = eval_output_path / f'eval_results_epoch{epoch}.json'
+                with open(fname, 'w') as f:
+                    json.dump(logs, f, indent=4)
+                # eval_logger.info(f'epoch: {epoch} eval acc: {logs["eval_accuracy"]} eval loss: {logs["eval_loss"]}')
+                eval_logger.info(f'epoch: {epoch} eval raw acc: {logs["eval_raw_accuracy"]} eval acc: {logs["eval_accuracy"]} eval loss: {logs["eval_loss"]}')
+
+class LogMetricsTrainer(Trainer):
+    def log_metrics(self, split, metrics):
+        if not self.is_world_process_zero():
+            return
+
+        logger.info(f"***** {split} metrics *****")
+        metrics_formatted = self.metrics_format(metrics)
+        k_width = max(len(str(x)) for x in metrics_formatted.keys())
+        v_width = max(len(str(x)) for x in metrics_formatted.values())
+        for key in sorted(metrics_formatted.keys()):
+            logger.info(f"  {key: <{k_width}} = {metrics_formatted[key]:>{v_width}}")
+
+@dataclass
+class DataCollatorForMultipleChoice:
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features):
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature.pop(label_name) for feature in features]
+        flattened_features = [
+            [
+                {k: v[i] for k, v in feature.items()}
+                for i in range(len(feature["input_ids"]))
+            ]
+            for feature in features
+        ]
+        flattened_features = sum(flattened_features, [])
+
+        batch = self.tokenizer.pad(
+            flattened_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        labels = sum(labels, [])
+        # batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        batch["labels"] = torch.tensor(labels, dtype=torch.int8)
+        return batch
+
+@dataclass
+class OldDataCollatorForMultipleChoice:
+    """
+    Data collator that will dynamically pad the inputs for multiple choice received.
+    """
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features):
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature.pop(label_name) for feature in features]
+        batch_size = len(features)
+        num_choices = len(features[0]["input_ids"])
+        flattened_features = [
+            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)] for feature in features
+        ]
+        flattened_features = sum(flattened_features, [])
+        batch = self.tokenizer.pad(
+            flattened_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+        return batch
---
+++ src/our_datasets.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import numpy as np
+from typing import Callable, Dict, List
+from dataclasses import dataclass, field
+
+@dataclass
+class Dataset:
+    tokenizer: Callable
+    delim: str = " [SEP] "
+    text_col: str = "text"
+    label_col: str = "label"
+    columns: List[str] = field(default_factory=list)
+    # TODO: Maybe add prefix. For boolq the question does not end
+    # With a '?' so maybe we should have a features to append a suffix
+    def __call__(self, example: Dict[str, str]) -> Dict[str, str]:
+        text = self.delim.join([example[label] for label in self.columns])
+        input = self.tokenizer(text)
+        input[self.text_col] = text # Save the text for analysis later.
+        # This is unneeded because label col is already in dataset
+        # input[self.label_col] = example[self.label_col]
+        return input
+
+@dataclass
+class WicGeneratedDataset(Dataset):
+    def __call__(self, example: Dict[str, str]) -> Dict[str, str]:
+        input = super().__call__(example)
+        input[self.label_col] = 1 if example['label'] == 'T' else 0
+        return input
+
+@dataclass
+class RteGeneratedDataset(Dataset):
+    def __call__(self, example: Dict[str, str]) -> Dict[str, str]:
+        input = super().__call__(example)
+        input[self.label_col] = 1 if example['label'] == 'entailment' else 0
+        return input
+
+@dataclass
+class BoolQGeneratedDataset(Dataset):
+    def __call__(self, example: Dict[str, str]) -> Dict[str, str]:
+        input = super().__call__(example)
+        input[self.label_col] = 1 if example['label'] else 0
+        return input
+
+# TODO: Finish implementing COPA dataset
+@dataclass
+class CopaOriginalDataset(Dataset):
+    def __call__(self, example: Dict[str, str]) -> Dict[str, str]:
+        return super().__call__(example)
+
+@dataclass
+class RecordOriginalDataset(Dataset):
+    query_col: str = "query"
+    entity_col: str = "entities"
+    answer_col: str = "answers"
+    passage_col: str = "passage"
+
+    def __call__(self, example: Dict[str, str]) -> Dict[str, str]:
+        ent_lengths = [len(ents) for ents in example['entities']]
+        passages = [[context] * ent_len for context, ent_len in zip(example['passage'], ent_lengths)]
+        query_headers = example['query']
+        endings = [
+            [f"{query} [SEP] {ent}" for ent in example['entities'][i]]
+            for i, query in enumerate(query_headers)
+        ]
+        passages = sum(passages, [])
+        endings = sum(endings, [])
+        output = self.tokenizer(passages, endings)
+        labels = np.zeros(len(passages), dtype=np.int8)
+    
+        ent_lengths.insert(0, 0)
+        ent_arr = np.array(ent_lengths)
+        length_sums = np.cumsum(ent_arr)
+    
+        for i, ans_list in enumerate(example['answers']):
+            for ans in ans_list:
+                ans_idx = example['entities'][i].index(ans)
+                labels[length_sums[i] + ans_idx] = 1
+        output[self.label_col] = labels
+        output[self.text_col] = [
+            passage + self.delim + end for passage, end in zip(passages, endings)
+        ]
+        return output
+
+@dataclass
+class RecordOriginalDatasetMC(Dataset):
+    query_col: str = "query"
+    entity_col: str = "entities"
+    answer_col: str = "answers"
+    passage_col: str = "passage"
+    num_choices: int = 3
+    def __call__(self, example: Dict[str, List[str]]) -> Dict[str, str]:
+        passages = [[passage] * self.num_choices for passage in example[self.passage_col]]
+        # TODO: Can this be done in a list comprehension
+        # TODO: Check if the label or correct query would always be last then?
+        queries = []
+        for i, query in enumerate(example[self.query_col]):
+            new_query = []
+            for j in range(self.num_choices - 1):
+                new_query.append(f"{query}{self.delim}{example[self.entity_col][i][j]}")
+            new_query.append(f"{query}{self.delim}{example[self.answer_col][i][0]}")
+
+            queries.append(new_query)
+
+        inputs = self.tokenizer(passages, queries)
+        inputs[self.label_col] = [self.num_choices - 1] * len(passages)
+
+        return {
+            k: [v[i: i+self.num_choices] for i in range(0, len(v), self.num_choices)]
+            for k, v in inputs.items()
+        }
+
+
+@dataclass
+class RecordOriginalDatasetVariableLength(Dataset):
+    query_col: str = "query"
+    entity_col: str = "entities"
+    passage_col: str = "passage"
+    def __call__(self, example: Dict[str, List[str]]) -> Dict[str, str]:
+        passages = [[ex] * len(example[self.entity_col][i])
+                    for i, ex in enumerate(example[self.passage_col])]
+        # Should we form queries this way? Or should we replace the @placeholder?
+        queries = [
+            [
+                f"{query}{self.delim}{entity}"
+                for entity in example[self.entity_col][i]
+            ]
+            for i, query in enumerate(example[self.query_col])
+        ]
+        
+        passages = sum(passages, [])
+        queries = sum(queries, [])
+
+        input = self.tokenizer(passages, queries)
+
+        # Output of tokenizer is the flattened input.
+        # We need to convert it back into a size of (N, E)
+        # Where N is the initial batch size and E is the number of entites
+        # For each problem, which changes.
+        i = 0
+        question_sizes = []
+        # Can this be done in a list comprehension?
+        for ex in example[self.entity_col]:
+            length = len(ex)
+            question_sizes.append((i, i + length))
+            i += length
+
+        tokenized = {
+            k: [v[start: end] for start, end in question_sizes]
+            for k, v in input.items()
+        }
+        tokenized['labels'] = [
+            [entity.index(ans) for ans in example['answers'][i]]
+            for i, entity in enumerate(example[self.entity_col])
+        ]
+
+        return tokenized
+
+@dataclass
+class RecordOriginalDatasetNoBatch(Dataset):
+    query_col: str = "query"
+    entity_col: str = "entities"
+    passage_col: str = "passage"
+    def __call__(self, example: Dict[str, List[str]]) -> Dict[str, str]:
+        texts = [
+            f"{example[self.passage_col]}{self.delim}{example[self.query_col]}"
+            + f"{self.delim}{entity}" for entity in example[self.entity_col]
+        ]
+        input = self.tokenizer(texts)
+
+        input['labels'] = [
+            example[self.entity_col].index(ans) for ans in example['answers']
+        ]
+
+        return input
